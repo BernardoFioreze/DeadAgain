9d88c57ff64dcfd3619bc3d0be850dd84e45d5b9
SaulDilly <saul.d@edu.pucrs.br>
2025-06-09 18:23:18 -0300

    Ajustado para o inventário ficar sempre visível

--- Scripts/inv_ui.gd
+++ Scripts/inv_ui.gd
@@ -5,8 +5,6 @@ class_name InvUi
 @onready var inv: Inv = preload("res://Inventory/playerinv.tres")
 @onready var slots: Array = $NinePatchRect/GridContainer.get_children()
 
-var is_open = false
-
 func _ready() -> void:
 	inv.update.connect(update_slots)
 	for i in range(slots.size()):
@@ -14,7 +12,7 @@ func _ready() -> void:
 		slots[i].slot_clicked.connect(_on_slot_clicked)
 		slots[i].drop_finished.connect(_on_drop_finished)
 	update_slots()
-	close()
+	visible = true
 	
 func update_slots(combinable_item: InvItem = null):
 	for i in range(min(inv.slots.size(), slots.size())):
@@ -33,22 +31,6 @@ func _on_slot_clicked(index: int):
 func get_selected_slot() -> InvSlot:
 	return inv.get_selected_slot()
 	
-	
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("i"):
-		if is_open:
-			close()
-		else:
-			open()
-
-func open():
-	visible = true
-	is_open = true
-
-func close():
-	visible = false
-	is_open = false
-	inv.selected_index = -1
 	
 func mark_combinable_slots(dragged_item: InvItem):
 	for i in range(min(inv.slots.size(), slots.size())):
